8d60ad7d43cc1ed20404a6185961ae8614ec78fe
JeanCamargoReal <jeancamargoreal@icloud.com>
2025-12-08 15:03:34 -0300

    build ApexPredator model

---
+++ JPApexPredators/ApexPredator.swift
@@ -0,0 +1,23 @@
+//
+//  ApexPredator.swift
+//  JPApexPredators
+//
+//  Created by Jean Camargo on 08/12/25.
+//
+
+struct ApexPredator: Decodable {
+    let id: Int
+    let name: String
+    let type: String
+    let latitude: Double
+    let longitude: Double
+    let movies: [String]
+    let movieScenes: [MovieScene]
+    let link: String
+    
+    struct MovieScene: Decodable {
+        let id: Int
+        let movie: String
+        let sceneDescription: String
+    }
+}
