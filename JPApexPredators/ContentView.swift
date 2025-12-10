776994bed30fdd55e1d44434421f3e88c6089b94
JeanCamargoReal <jeancamargoreal@icloud.com>
2025-12-10 15:25:27 -0300

    List

--- JPApexPredators/ContentView.swift
+++ JPApexPredators/ContentView.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    let predators = Predators()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(predators.apexPredators) { predators in
+            HStack{
+                // Dinosaur imagme
+                Image(predators.image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                    .shadow(color: .white, radius: 1)
+                
+                VStack(alignment: .leading) {
+                    // Name
+                    Text(predators.name)
+                        .fontWeight(.bold)
+                    
+                    // Type
+                    Text(predators.type.rawValue.capitalized)
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .padding(.horizontal, 13)
+                        .padding(.vertical, 5)
+                        .background(predators.type.background)
+                        .clipShape(.capsule)
+                }
+            }
         }
-        .padding()
+        .preferredColorScheme(.dark)
     }
 }
 
