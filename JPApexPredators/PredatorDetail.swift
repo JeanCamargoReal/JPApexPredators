ebd5f5fbada1e6813e3b175b7b1da1568550672f
JeanCamargoReal <jeancamargoreal@icloud.com>
2026-01-02 10:47:55 -0300

    zoom transition

--- JPApexPredators/PredatorDetail.swift
+++ JPApexPredators/PredatorDetail.swift
@@ -13,6 +13,8 @@ struct PredatorDetail: View {
     
     @State var position: MapCameraPosition
     
+    @Namespace var namespace
+    
     var body: some View {
         GeometryReader { geo in
             ScrollView {
@@ -48,7 +50,18 @@ struct PredatorDetail: View {
                     
                     // Current location
                     NavigationLink {
-                        
+                        PredatorMap(
+                            position:
+                                    .camera(
+                                        MapCamera(
+                                            centerCoordinate: predator.location,
+                                            distance: 1000,
+                                            heading: 250,
+                                            pitch: 80
+                                        )
+                                    )
+                            )
+                        .navigationTransition(.zoom(sourceID: 1, in: namespace))
                     } label: {
                         Map(position: $position) {
                             Annotation(
@@ -77,6 +90,7 @@ struct PredatorDetail: View {
                         }
                         .clipShape(.rect(cornerRadius: 15))
                     }
+                    .matchedTransitionSource(id: 1, in: namespace)
                     
                     // Appears in
                     Text("Appears In:")
